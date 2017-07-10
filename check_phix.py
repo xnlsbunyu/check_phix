cbd8f2137007af4506fc573e4d3647e9fb824805
Xianan Liu <xl586@nyu.edu>
2017-07-10 11:46:25 -0400

    add the script

---
+++ check_phix.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# Check the phix reads in fastq file
+# bwa and phix reference genome need to be installed and indexed first
+
+import sys
+import os
+
+def main():
+    forward = sys.argv[1]
+
+
+    #Download the PhiX174 reference genome from NCBI
+    print ("Getting PhiX sequence from NCBI......")
+    os.system("curl ftp://ftp.ncbi.nlm.nih.gov/genomes/Viruses/Enterobacteria_phage_phiX174_sensu_lato_uid14015/NC_001422.fna > phix174.fasta")
+
+    #Build phix index using bwa
+    os.system("bwa index -p phix -a bwtsw phix174.fasta")
+    
+    #Map fastq reads to phix genome
+    os.system("bwa mem -M phix " + forward +" > tem.sam")
+   
+    print("The total number of reads is .......")
+    os.system("cat " + forward + " | echo $((`wc -l`/4))")
+
+    #find mapped reads in sam file
+    print ("Unmapped reads number is ......")
+    os.system("awk '/\*/' tem.sam | wc -l")
+    os.system("rm phix174.fasta")
+    os.system("rm phix*")
+    os.system("rm tem.sam")
+
+
+if __name__ == "__main__":
+    main()
