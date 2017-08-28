fd8199e0c0af4135cce07c996f33de1cca5a0aae
Xianan Liu <xl586@nyu.edu>
2017-08-28 18:34:22 -0400

    add bash script to extract sequence record of yeast genome

--- check_s288c.py
+++ check_s288c.py
@@ -26,8 +26,13 @@ def main():
     print("The total number of reads is .......")
     os.system("cat " + forward + " | echo $((`wc -l`/4))")
 
+    os.system("awk '{if ($3 !~ /\*/) print}' tem.sam | awk '{if ($2 <= 16) print}' | grep -v \^\@ > mapped.txt")
     print ("Mapped reads number is .......")
-    os.system("awk '{if ($3 !~ /\*/) print}' tem.sam | awk '{if ($2 <= 16) print}' | grep -v \^\@ | wc -l")
+    os.system("wc -l mapped.txt")
+    #Extract the sequence ids from the mapped.txt (The first field) and add the @ at the beginning of the line
+    os.system("awk '{print $1}' mapped.txt | sed 's/^/@/' > id_list.txt ")
+    #grep the sequence read (all four lines) by the id list
+    os.system("grep -A 3 -f id_list.txt " + forward + " | sed '/^--$/d' > yeast_reads.fastq")
     os.system("rm S288C_reference_sequence_R64-2-1_20150113.fsa")
     os.system("rm s288c*")
     os.system("rm tem.sam")
